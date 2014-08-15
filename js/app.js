69bdeba8158240e4cb2b2e600aa21bba44f83564
Dannzzor <ddavenport420@gmail.com>
2014-08-15 10:26:45 -0700

    remove client info

--- js/app.js
+++ js/app.js
@@ -108,26 +108,8 @@ var myApp = angular.module('DRDapp', ['myFilters'])
    $scope.params = $routeParams;
 
    $scope.clientList = [
-     {url: 'http://jesps.jecsi.com/mppg_variance/default.aspx', imgUrl:'images/amcor-orora-(2)-small.png', name: 'Amcor / Orora', version: '2.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/BAXTER/default.aspx', imgUrl:'images/baxter-(3)-small.png', name: 'Baxter', version: '3.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/BAKERH/default.aspx', imgUrl:'images/bakerh-(jetro)-small.png', name: 'Baker Hughes', version: 'JETRO', unc: "\\ondemand.jecsi.com\clients\bakerh"},
-     {url: 'http://it.jecsi.com/jesqma/default.aspx', imgUrl:'images/jesqma-jecs-(2)-small.png', name: 'JESQMA', version: '4.0', unc: ""},
-     {url: 'http://www.ktxcwaste.net/default.aspx', imgUrl:'images/toxco-retriev-ktxc-(2)-small.png', name: 'Toxco / Retriev / KTXC', version: '2.0', unc: ""},
-     {url: 'http://www.ktrlwaste.net/default.aspx', imgUrl:'images/toxco-retriev-ktxc-(2)-small.png', name: 'Toxco / Retriev / KTRL', version: '2.0', unc: ""},
-     {url: 'http://jesharepoint.jecsi.com/sites/jets/default.aspx', imgUrl:'images/jet-(2)-small.png', name: 'JET', version: '2.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/sites/jets/default.aspx', imgUrl:'images/jet-(jetro)-small.png', name: 'JET', version: 'JETRO', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/vanf/default.aspx', imgUrl:'images/vanf-(3)-small.png', name: 'VANF', version: '3.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/jank/default.aspx', imgUrl:'images/jank-(3)-small.png', name: 'JANK', version: '3.0', unc: ""},
-     {url: 'http://jesharepoint.jecsi.com/sites/TUBE/default.aspx', imgUrl:'images/tube-(2)-small.png', name: 'Tube 2.0', version: '2.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/tube/default.aspx', imgUrl:'images/tube-(3)-small.png', name: 'Tube 3.0', version: '3.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/gemxtx/default.aspx', imgUrl:'images/ge-(jetro)-small.png', name: 'GE', version: 'JETRO', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/mkln/default.aspx', imgUrl:'images/markland-(3)-small.png', name: 'Markland', version: '3.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/amfabr/default.aspx', imgUrl:'images/ams-(3)-small.png', name: 'AMS', version: '3.0', unc: ""},
-     {url: 'http://jesharepoint.jecsi.com/clients/VMET/default.aspx', imgUrl:'images/vmet-(2)-small.png', name: 'VMET / Vista Metals', version: '2.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/vmetal/default.aspx', imgUrl:'images/vmet-(3)-small.png', name: 'VMET / Vista Metals', version: '3.0', unc: ""},
-     {url: 'http://wilbur.ondemandehs.net/default.aspx', imgUrl:'images/wilbur-(3)-small.png', name: 'Wilbur', version: '3.0', unc: ""},
-     {url: 'http://ondemand.jecsi.com/clients/HALB/default.aspx', imgUrl:'images/haliburton-(jetro)-small.png', name: 'Haliburton', version: 'JETRO', unc: ""},
-     {url: 'https://www.jet-bso.com/default.aspx', imgUrl:'images/jetbso-(3)-small.png', name: 'JET BSO', version: '3.0', unc: ""}
+     {url: '#', imgUrl:'', name: 'test', version: '2.0', unc: ""},
+     {url: '#', imgUrl:'', name: 'test2', version: '3.0', unc: ""}
    ];
 })
 
@@ -138,33 +120,6 @@ var myApp = angular.module('DRDapp', ['myFilters'])
   $scope.params = $routeParams;
 
   $scope.links = [
-    {url: 'http://jesps.jecsi.com/mppg_variance/default.aspx',        type: 'Client', name: 'Amcor / Orora',          details: '2.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/BAXTER/default.aspx',    type: 'Client', name: 'Baxter',                 details: '3.0',   unc: '\\\\ondemand.jecsi.com\\clients\\baxter'},
-    {url: 'http://ondemand.jecsi.com/clients/BAKERH/default.aspx',    type: 'Client', name: 'Baker Hughes',           details: 'JETRO', unc: '\\\\ondemand.jecsi.com\\clients\\bakerh'},
-    {url: 'http://it.jecsi.com/jesqma/default.aspx',                  type: 'Client', name: 'JESQMA',                 details: '4.0',   unc: ''},
-    {url: 'http://www.ktxcwaste.net/default.aspx',                    type: 'Client', name: 'Toxco / Retriev / KTXC', details: '2.0',   unc: ''},
-    {url: 'http://www.ktrlwaste.net/default.aspx',                    type: 'Client', name: 'Toxco / Retriev / KTRL', details: '2.0',   unc: ''},
-    {url: 'http://jesharepoint.jecsi.com/sites/jets/default.aspx',    type: 'Client', name: 'JET',                    details: '2.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/sites/jets/default.aspx',        type: 'Client', name: 'JET',                    details: 'JETRO', unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/vanf/default.aspx',      type: 'Client', name: 'VANF',                   details: '3.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/jank/default.aspx',      type: 'Client', name: 'JANK',                   details: '3.0',   unc: ''},
-    {url: 'http://jesharepoint.jecsi.com/sites/TUBE/default.aspx',    type: 'Client', name: 'Tube 2.0',               details: '2.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/tube/default.aspx',      type: 'Client', name: 'Tube 3.0',               details: '3.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/gemxtx/default.aspx',    type: 'Client', name: 'GE',                     details: 'JETRO', unc: '\\\\ondemand.jecsi.com\\clients\\gemxtx'},
-    {url: 'http://ondemand.jecsi.com/clients/mkln/default.aspx',      type: 'Client', name: 'Markland',               details: '3.0',   unc: '\\\\ondemand.jecsi.com\\clients\\mkln'},
-    {url: 'http://ondemand.jecsi.com/clients/amfabr/default.aspx',    type: 'Client', name: 'AMS',                    details: '3.0',   unc: ''},
-    {url: 'http://jesharepoint.jecsi.com/clients/VMET/default.aspx',  type: 'Client', name: 'VMET / Vista Metals',    details: '2.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/vmetal/default.aspx',    type: 'Client', name: 'VMET / Vista Metals',    details: '3.0',   unc: ''},
-    {url: 'http://wilbur.ondemandehs.net/default.aspx',               type: 'Client', name: 'Wilbur',                 details: '3.0',   unc: ''},
-    {url: 'http://ondemand.jecsi.com/clients/HALB/default.aspx',      type: 'Client', name: 'Haliburton',             details: 'JETRO', unc: '\\\\ondemand.jecsi.com\\clients\\halb'},
-    {url: 'https://www.jet-bso.com/default.aspx',                     type: 'Client', name: 'JET BSO',                details: '3.0',   unc: ''},
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    {url: 'http://ondemand.jecsi.com/sites/resources/default.aspx', type: 'Internal', name: 'Sites Resources', details: 'Resources', unc: '\\\\ondemand.jecsi.com\\sites\\resources'},
-    {url: 'http://webapps.jecsi.com/DbNetGrid/publish/cleangriddemo/cleangriddemo.aspx', type: 'Internal', name: 'Clean Grid Demo', details: 'Grid', unc: '\\\\jeportal\\DbNetLink\\DbNetSuite 3.3\\DbNetGrid\\publish\\cleangriddemo'},
-    {url: 'http://it.jecsi.com/demo/dynamic/Web/FAQ.aspx', type: 'Internal', name: 'FAQs Demo', details: 'Template', unc: ''},
-    {url: 'http://it.jecsi.com/demo/dynamic/Web/Navigation.aspx', type: 'Internal', name: 'Navigation Demo', details: 'Template', unc: ''},
-    {url: 'http://it.jecsi.com/demo/dynamic/Web/Settings.aspx', type: 'Internal', name: 'Settings / Reports Demo', details: 'Template', unc: ''},
-    {url: 'http://webapps.ondemandehs.net/dbnetsuite/help/dbnetsuite.htm?client_side_programming.htm', type: 'Reference', name: 'DbNetGrid Online Manual', details: '3.x', unc: ''},
     {url: 'http://oscarotero.com/jquery/', type: 'Reference', name: 'jQuery Cheatsheet', details: 'JS', unc: ''},
     {url: 'http://jshint.com/', type: 'Testing', name: 'JS Hint', details: 'JS', unc: ''},
     {url: 'http://www.regular-expressions.info/javascriptexample.html', type: 'Reference', name: 'Regular-Expression (regex) Builder', details: 'JS', unc: ''},
@@ -173,9 +128,7 @@ var myApp = angular.module('DRDapp', ['myFilters'])
     {url: 'http://javascriptweblog.wordpress.com/2011/02/07/truth-equality-and-javascript/', type: 'Reference', name: 'JavaScript: Truth and Equality', details: 'JS', unc: ''},
     {url: '', type: '', name: '', details: '', unc: ''},
     {url: '', type: '', name: '', details: '', unc: ''},
-    {url: '', type: '', name: '', details: '', unc: ''},
-    {url: 'http://it.jecsi.com/itsps/Lists/SPSDepartmentWork/AllItems.aspx', type: 'Internal', name: 'SPS Department Work List', details: 'Tasks', unc: ''}
-
+    {url: '', type: '', name: '', details: '', unc: ''}
   ];
 })
 
